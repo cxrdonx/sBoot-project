c15575904eeaa4cc7b50b7f625c184ceb84eee99
José Cardona <jose.eduardo.cardona@gmail.com>
2021-12-15 19:01:28 -0600

    uso de anotacion value para apuntar a properties

--- src/main/java/com/springBoot/example/spring/boot/configuration/GeneralConfiguration.java
+++ src/main/java/com/springBoot/example/spring/boot/configuration/GeneralConfiguration.java
@@ -8,11 +8,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
@@ -21,6 +23,15 @@ public class GeneralConfiguration {
     private String apellido;
     @Value("${value.random}")
     private String random;
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${driver}")
+    private String driver;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name, apellido);
@@ -29,10 +40,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
